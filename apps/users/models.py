4bef0ac94320d8eef5925d565fa591c41e317c9a
xiangxin2001 <2868308648@qq.com>
2022-05-09 15:06:03 +0800

    logo.png

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class USer(models.Model):
+    username=models.CharField(max_length=20)
+    password=models.CharField(max_length=20)
+    mobile=models.CharField(max_length=11,unique=True)
